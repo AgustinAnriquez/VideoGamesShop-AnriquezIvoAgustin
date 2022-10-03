32bfd8359aadf8f14664e3359474a83c98f72238
AgustinAnriquez <agustin.anriquez98@gmail.com>
2022-10-03 16:14:02 -0300

    se agrega boton añadir al carrito

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,18 +1,24 @@
 import Card from 'react-bootstrap/Card';
 import ItemCount from './ItemCount';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Container } from 'react-bootstrap';
 
 const ItemDetail = ({product}) =>{
     return(
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={product.pictureUrl} />
-        <Card.Body>
-          <Card.Title>{product.title}</Card.Title>
-          <Card.Text>
-            {product.description}
-          </Card.Text>
-          <ItemCount stock={product.stock}/>
-        </Card.Body>
-      </Card>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={product.pictureUrl} />
+      <Card.Body>
+        <Card.Title>{product.title}</Card.Title>
+        <Card.Text>
+        {product.description}
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item><ItemCount stock={product.stock}/></ListGroup.Item>
+        <ListGroup.Item><Button variant="primary">Agregar al carrito</Button></ListGroup.Item>
+      </ListGroup>    
+    </Card>
     )
 }
 
--- src/components/products.js
+++ src/components/products.js
@@ -1,7 +1,7 @@
 const products = [
-    {id: 1, categoryId: 1, title:'Play Station 4', description:'string', stock:5, price: 2000, pictureUrl:'https://http2.mlstatic.com/D_NQ_NP_797628-MLA31063125323_062019-V.jpg'},
-    {id: 2, categoryId: 1, title:'Play Station 5', description:'string', stock:5, price: 2000, pictureUrl:'https://basic-tutorials.de/wp-content/uploads/2020/09/PlayStation-5.jpg'},
-    {id: 3, categoryId: 2, title:'Kingdom Hearts 3', description:'string', stock:5, price: 2000, pictureUrl:'https://images.khinsider.com/2018%20Uploads/12/KH3%20Japan%20Box%20Cover.jpg'}
+    {id: 1, categoryId: 1, title:'Play Station 4', description:'Con tu consola PlayStation 4 tendrás entretenimiento asegurado todos los días. Su tecnología fue creada para poner nuevos retos tanto a jugadores principiantes como expertos. ', stock:5, price: 2000, pictureUrl:'https://http2.mlstatic.com/D_NQ_NP_797628-MLA31063125323_062019-V.jpg'},
+    {id: 2, categoryId: 1, title:'Play Station 5', description:'PlayStation renovó las expectativas del mundo virtual con esta nueva consola y su gran rendimiento. Cuenta con una interfaz de usuario más rápida y fácil de navegar que en anteriores modelos. Además, podrás jugar durante horas desafiando a millones de contrincantes alrededor del mundo que esperan nuevos retos.', stock:5, price: 2000, pictureUrl:'https://basic-tutorials.de/wp-content/uploads/2020/09/PlayStation-5.jpg'},
+    {id: 3, categoryId: 2, title:'Kingdom Hearts 3', description:'Con este juego de Kingdom Hearts vas a disfrutar de horas de diversión y de nuevos desafíos que te permitirán mejorar como gamer', stock:5, price: 2000, pictureUrl:'https://images.khinsider.com/2018%20Uploads/12/KH3%20Japan%20Box%20Cover.jpg'}
   ]
 
 export const getProducts = () =>{

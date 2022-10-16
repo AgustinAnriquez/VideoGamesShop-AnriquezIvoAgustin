7880b099d628b9979fc68e5d3d0e13e67e9dfc13
AgustinAnriquez <agustin.anriquez98@gmail.com>
2022-10-16 16:45:29 -0300

    se agrega funcion onAdd en item detail

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -4,28 +4,29 @@ import MinusWidget from './MinusWidget'
 import Button from 'react-bootstrap/Button'
 import './ItemCount.css'
 
-const ItemCount = ({stock}) =>{
+const ItemCount = ({stock, onAdd}) =>{
   // Declara una nueva variable de estado, la cual llamaremos “count”
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
 
   const addCount = () =>{
-    if(count < stock){
-      setCount(count + 1)
-    }
+    setCount(count + 1)
   }
-
   const disCount = () =>{
-    if(count > 0){
-      setCount(count - 1)
-    }
+    setCount(count - 1)
   }
 
 
   return (
     <div className='countContainer'>
-      <Button variant="outline-danger" onClick={disCount}><MinusWidget/></Button>
-      <span className='count'>{count}</span>
-      <Button variant="outline-primary" onClick={addCount}><PlusWidget/></Button>
+        <div className='listCount'>
+            <Button variant="outline-danger" disabled={count <= 1} onClick={disCount}><MinusWidget/></Button>
+            <span className='count'>{count}</span>
+            <Button variant="outline-primary" disabled={count >= stock} onClick={addCount}><PlusWidget/></Button>
+        </div>
+        <div className='listCount'>
+          <Button variant="primary" disabled={stock <= 0} onClick={onAdd}>Agregar al carrito</Button>
+        </div>
+          
     </div>
   );
 }
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,24 +1,37 @@
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import ItemCount from './ItemCount';
-import Button from 'react-bootstrap/Button';
-import ListGroup from 'react-bootstrap/ListGroup';
-import { Container } from 'react-bootstrap';
+import './ItemDetail.css'
+import { Link } from "react-router-dom";
 
 const ItemDetail = ({product}) =>{
+
+    const [showItemCount, setShowItemCount] = useState(true)
+
+    const onAdd = () =>{
+      setShowItemCount(false)
+    }
+
     return(
+      <div className='itemDetail'>
       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={product.pictureUrl} />
-      <Card.Body>
-        <Card.Title>{product.title}</Card.Title>
-        <Card.Text>
-        {product.description}
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item><ItemCount stock={product.stock}/></ListGroup.Item>
-        <ListGroup.Item><Button variant="primary">Agregar al carrito</Button></ListGroup.Item>
-      </ListGroup>    
-    </Card>
+        <Card.Img variant="top" src={product.pictureUrl} />
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>
+          {product.description}
+          </Card.Text>
+        </Card.Body>
+        </Card>
+      {
+        showItemCount ? <ItemCount stock={product.stock} onAdd={onAdd}/> : 
+        <Link to='/cart'>
+          <Button className='goCartButton' variant="success">Ir al carrito</Button>
+        </Link> 
+      }
+      </div>
+
     )
 }
 

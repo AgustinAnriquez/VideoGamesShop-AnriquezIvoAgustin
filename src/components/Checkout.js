f3e71882d60471cd422da4337bf42a278c8d9955
AgustinAnriquez <agustin.anriquez98@gmail.com>
2022-10-29 16:14:03 -0300

    se agrega checkout a cart

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,5 +1,20 @@
+import { useContext} from "react";
+import CartContext from "../context/CartContext";
 const Checkout = () => {
-    return ( <p>Check</p> );
+    const {cart, total} = useContext(CartContext)
+    return ( 
+    <div>
+        <h4>Productos</h4>
+        <ul>
+        {cart.map((item) =>(
+                <li key={item.id}>
+                  <span>{item.quantity} ${item.price} {item.title}</span>
+                </li>
+              ))}
+        </ul>
+        <h4>Total: ${total}</h4>
+    </div>
+    );
 }
  
 export default Checkout;
--- src/components/OrderModal.js
+++ src/components/OrderModal.js
@@ -1,20 +1,44 @@
 import { Alert, Button, Form, Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Checkout from "./Checkout";
+import { useState} from "react";
+import './OrderModal.css'
 
 const OrderModal = ({showModal, onClose, onBuy, orderId}) =>{
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [email2, setEmail2] = useState('');
+
+    function onNameChange(evt) {
+        setName(evt.target.value);
+    }
+    function onPhoneChange(evt) {
+        setPhone(evt.target.value);
+    }
+    function onEmailChange(evt) {
+        setEmail(evt.target.value);
+    }
+    function onEmail2Change(evt) {
+        setEmail2(evt.target.value);
+    }
+    const isDisabled = !(name !== '' && phone !== '' && email !== '' && email2 !== '' && email === email2) 
     return(
         <Modal show={showModal} onHide={onClose}>
           <Modal.Header closeButton>
             <Modal.Title>Finalizar compra</Modal.Title>
           </Modal.Header>
-          <Modal.Body>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Correo</Form.Label>
-              <Form.Control type="email" placeholder="Ingrese correo" />
+          <Modal.Body className="modalContainer">
+            <Checkout className="checkoutContainer"/>
+            <Form.Group className="formContainer" controlId="formBasicEmail">
               <Form.Label>Nombre</Form.Label>
-              <Form.Control type="text" placeholder="Ingrese nombre" />
+              <Form.Control type="text" placeholder="Ingrese nombre" onChange={(evt) => onNameChange(evt)}/>
               <Form.Label>Telefono</Form.Label>
-              <Form.Control type="number" placeholder="Ingrese telefono" />
+              <Form.Control type="number" placeholder="Ingrese telefono" onChange={(evt) => onPhoneChange(evt)}/>
+              <Form.Label>Correo</Form.Label>
+              <Form.Control type="email" placeholder="Ingrese correo" onChange={(evt) => onEmailChange(evt)}/>
+              <Form.Label>Repetir correo</Form.Label>
+              <Form.Control type="email" placeholder="Ingrese correo" onChange={(evt) => onEmail2Change(evt)}/>
             </Form.Group>
           </Modal.Body>
           <Modal.Footer>
@@ -24,7 +48,7 @@ const OrderModal = ({showModal, onClose, onBuy, orderId}) =>{
                         <Button variant="secondary" onClick={onClose}>
                             Cancelar
                         </Button>
-                        <Button variant="primary" onClick={onBuy}>
+                        <Button variant="primary" onClick={onBuy} disabled={isDisabled}>
                             Comprar
                         </Button>
                     </>
